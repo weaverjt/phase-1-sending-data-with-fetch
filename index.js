2501a85adfde3365568d64d1391ff40f4534191c
Jacob <weaver.jacob.t@gmail.com>
2023-12-04 16:46:42 -0600

    here tag

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+
+const formData = {
+    name: "Steve",
+    email: "steve@steve.com",
+  };
+  
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  function submitData (name, email) {
+
+    name = formData.name;
+    email = formData.email;
+    let here = document.getElementById("here");
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+       
+        here.append(object.id);
+    })
+    .catch(function (error) {
+      alert("Unauthorized Access");
+      here.append(error.message);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", submitData);
